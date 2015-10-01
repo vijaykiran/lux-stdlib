fb0932b347b671c5702e439a97621974f5a3c4cc
Eduardo Julian <eduardoejp@gmail.com>
2015-10-01 10:54:52 -0400

    First commit!

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject lux/stdlib "0.3.0"
+  :description "Standard library for the Lux family of programming languages."
+  :url "https://github.com/LuxLang/stdlib"
+  :license {:name "Mozilla Public License (Version 2.0)"
+            :url "https://www.mozilla.org/en-US/MPL/2.0/"}
+  :plugins [[lux/lein "0.1.0"]]
+  )
